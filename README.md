2b34a48236522a0d67b568cf6e7c7c052c3a0522
WRting <1348125370@qq.com>
2019-09-10 19:59:11 +0800

    the first demo

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Express-study
+
+```javascript
+// 1. 安装
+
+//2. 引包
+
+const express = require('express')
+
+//3. 创建服务器应用程序
+
+const app = express()
+
+//当服务器收到get/请求的时候，执行回调处理函数
+
+app.get('/', (req, res) => res.send('Hello World!'))
+/*或者这样写
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+})
+*/
+
+//相当于server.listen
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+```
+
---
+++ app.js
@@ -0,0 +1,36 @@
+// 1. 安装
+
+//2. 引包
+
+const express = require('express')
+
+//3. 创建服务器应用程序
+
+const app = express()
+
+//公开指定目录
+app.use('/public/',express.static('./public/'))
+
+//当服务器收到get/请求的时候，执行回调处理函数
+
+app.get('/', function(req, res) {
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+       <h1> hello express!
+    </body>
+    </html>
+    `)
+})
+
+
+//相当于server.listen
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
